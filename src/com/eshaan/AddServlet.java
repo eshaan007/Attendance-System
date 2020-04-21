5aa911e5f3e847727be09667dfba8560e67f5785
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-04-21 17:09:11 +0530

    Session and Cookie

--- src/com/eshaan/AddServlet.java
+++ src/com/eshaan/AddServlet.java
@@ -5,19 +5,28 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-		res.sendRedirect("sq?k="+k);
+		Cookie cookie = new Cookie("k", k + "");
+		res.addCookie(cookie);
+		
+//		HttpSession session = req.getSession();
+//		session.setAttribute("k", k);
+		
+		res.sendRedirect("sq");
 		
 		//Session management
 		
--- src/com/eshaan/SqServlet.java
+++ src/com/eshaan/SqServlet.java
@@ -3,20 +3,31 @@ package com.eshaan;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
 		
-		int k = Integer.parseInt(req.getParameter("k"));
+//		HttpSession session = req.getSession();	
+//		int k = (int)session.getAttribute("k");
 		
-		k= k * k;
+		int k = 0;	
+		Cookie cookies[] = req.getCookies();
+		
+		for(Cookie c : cookies) {
+			if(c.getName().equals("k"))
+				k = Integer.parseInt(c.getValue());
+		}
+			
+		k = k * k;
 		
 		PrintWriter out = res.getWriter();
-		out.println("Result is "+k);
+		out.println("Result is " + k);
 		
 		System.out.println("sq called");
 	}
