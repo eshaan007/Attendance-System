c7b54a9479c55276fc6ea01d3dfff88360c04f48
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-04-21 23:01:38 +0530

    Servlet Annotations

--- src/com/eshaan/AddServlet.java
+++ src/com/eshaan/AddServlet.java
@@ -5,12 +5,14 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
--- src/com/eshaan/SqServlet.java
+++ src/com/eshaan/SqServlet.java
@@ -3,12 +3,14 @@ package com.eshaan;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/sq")
 public class SqServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
